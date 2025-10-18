4eb545bcfdce1ad65d2ca538d2ffed3523d2fb23
Charliekofg <charliekofg@hotmail.com>
2025-10-18 20:04:56 +0000

    tbd:improve layout

--- src/js/components/Lights.jsx
+++ src/js/components/Lights.jsx
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
 
 const Lights = () => {
-	const [ selectedColor, setSelectedColor ] = useState ("yellow")
-    return (
-        <div className="traffic-light">
+	const [selectedColor, setSelectedColor] = useState("yellow")
+	return (
 
-			
-			<div
-			onClick= {() => setSelectedColor ("red")} 
-			className={
-				"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+		<div className="traffic-light-wrapper">
+			<div className="tube"></div>
 
-			
-			<div 
-			onClick={() => setSelectedColor ("yellow")}
-			className={
-			"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+			<div className="traffic-light">
 
-			
-			<div
-			onClick={() => setSelectedColor ("green")}
-			className={
-			"light green" + (selectedColor === "green" ? " glow" : "")}></div>
-            
 
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={
+						"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+
+
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={
+						"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+
+
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={
+						"light green" + (selectedColor === "green" ? " glow" : "")}></div>
+
+
+			</div>
 		</div>
-    )
+	)
 };
 
 export default Lights;
