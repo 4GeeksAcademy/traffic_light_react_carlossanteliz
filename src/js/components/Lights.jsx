a48cadad7533a071be24fe6241122f996c0a8e23
Charliekofg <charliekofg@hotmail.com>
2025-10-18 19:42:26 +0000

    logic done_tbd:finish layout and positioning

--- src/js/components/Lights.jsx
+++ src/js/components/Lights.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Lights = () => {
+	const [ selectedColor, setSelectedColor ] = useState ("yellow")
     return (
         <div className="traffic-light">
-			<div className="light red"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
+
+			
+			<div
+			onClick= {() => setSelectedColor ("red")} 
+			className={
+				"light red" + (selectedColor === "red" ? " glow" : "")}></div>
+
+			
+			<div 
+			onClick={() => setSelectedColor ("yellow")}
+			className={
+			"light yellow" + (selectedColor === "yellow" ? " glow" : "")}></div>
+
+			
+			<div
+			onClick={() => setSelectedColor ("green")}
+			className={
+			"light green" + (selectedColor === "green" ? " glow" : "")}></div>
             
 
 		</div>
